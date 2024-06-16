ec1cee5316167609f4fbbb600767ddefcaa9ffb5
tzito <tzito93@gmail.com>
2024-06-16 11:16:22 -0400

    exercise 1.14

---
+++ The C Programming Language Book/Chapter 1/exercise_1_14.c
@@ -0,0 +1,38 @@
+/* Prints a histogram of the frequencies of different characters in the input. */
+
+
+#include <stdio.h>
+
+/* 94 visible characters in the Standard ASCII chart, not including space */
+#define ARRLEN 94
+#define OFFSET 33
+
+main()
+{
+    int frequencies[ARRLEN];
+    int c, i, j;
+
+    printf("This program will print a histogram of the frequencies of each character in the input.\n\n");
+    printf("When you are done typing in the input, press ENTER and CTRL+D to print the histogram.\n");
+    printf("Each dash represents one occurence of the given character.\n");
+
+    /* Set each index in the array to 0 */
+    for (i = 0; i < ARRLEN; ++i)
+        frequencies[i] = 0;
+
+    /* Update frequencies for each char until EOF */
+    while ((c = getchar()) != EOF) {
+        ++frequencies[c - OFFSET];
+    }
+
+    /* Print the histogram */
+    for (i = 0; i < ARRLEN; ++i) {
+        putchar(i + OFFSET);
+        printf(":");
+
+        for (j = 0; j < frequencies[i]; ++j)
+            printf(" -");
+
+        printf("\n");
+    }
+}
