ec919cf7fad08de62b0892a1ec0294f3624d08f3
tzito <tzito93@gmail.com>
2024-06-16 15:11:48 -0400

    exercise 1.15

--- The C Programming Language Book/Chapter 1/exercise_1_14.c
+++ The C Programming Language Book/Chapter 1/exercise_1_14.c
@@ -7,14 +7,14 @@
 #define ARRLEN 94
 #define OFFSET 33
 
-main()
+int main()
 {
     int frequencies[ARRLEN];
     int c, i, j;
 
     printf("This program will print a histogram of the frequencies of each character in the input.\n\n");
     printf("When you are done typing in the input, press ENTER and CTRL+D to print the histogram.\n");
-    printf("Each dash represents one occurence of the given character.\n");
+    printf("Each dash represents one occurrence of the given character.\n");
 
     /* Set each index in the array to 0 */
     for (i = 0; i < ARRLEN; ++i)
@@ -35,4 +35,6 @@ main()
 
         printf("\n");
     }
+
+    return 0;
 }
--- The C Programming Language Book/Chapter 1/hello_world.c
+++ The C Programming Language Book/Chapter 1/hello_world.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-main() {
+int main() {
     printf("%d + %d = %d\n", 5, 5, 10);
     int myChar = 'c';
     printf("my char is %c", myChar);
